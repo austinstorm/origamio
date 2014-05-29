35e52ee23b910e7e7dd2c892adf4e0bb3c8f38d1
austinstorm@gmail.com <iknowthis>
2014-05-29 08:42:08 -0700

    beginning of signin template

--- wsgi/origamio/urls.py
+++ wsgi/origamio/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'origamio.views.home', name='home'),
     # url(r'^openshift/', include('openshift.foo.urls')),
+    url(r'^signin/', 'origamio.views.signin', name='signin'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
