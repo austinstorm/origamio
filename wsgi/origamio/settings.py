ebbb9c2969b58d55e4ab213cf4c521311276f216
austinstorm@gmail.com <iknowthis>
2014-05-29 01:32:08 -0700

    fix deploy folder names

--- wsgi/origamio/settings.py
+++ wsgi/origamio/settings.py
@@ -17,7 +17,7 @@ else:
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Austin Storm', 'austinstorm@gmail.com'),
 )
 MANAGERS = ADMINS
 
@@ -53,7 +53,7 @@ else:
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
